8ff91ed262617e2f40a568af19d2bc39627e249a
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2017-12-26 22:13:55 -0500

    REVIEW: moved both navigators (tabs and stack) in to separate files for cleaner code

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native'
 import {purple, white} from './utils/colors'
-import { TabNavigator, StackNavigator } from 'react-navigation'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { Constants } from 'expo'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
-import DeckList from './components/views/DeckList'
-import DeckDetail from './components/views/DeckDetail'
-import AddDeck from './components/views/AddDeck'
-import AddCard from './components/views/AddCard'
-import Quiz from './components/views/Quiz'
+import MainNavigator from './components/views/MainNavigator'
 import reducer from './reducers'
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
@@ -21,74 +16,6 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
         </View>
     )
 }
-
-const Tabs = TabNavigator({
-    DeckList: {
-        screen: DeckList,
-        navigationOptions: {
-            tabBarLabel: 'Decks',
-            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-        }
-    },
-    AddDeck: {
-        screen: AddDeck,
-        navigationOptions: {
-            tabBarLabel: 'New Deck',
-            tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-        }
-    }
-}, {
-    navigationOptions: {
-        header: null
-    },
-    tabBarOptions: {
-        activeTintColor: Platform.OS === 'ios' ? purple : white,
-        style: {
-            height: 56,
-            backgroundColor: Platform.OS === 'ios' ? white : purple,
-            shadowColor: 'rgba(0, 0, 0, 0.24)',
-            shadowOffset: {
-                width: 0,
-                height: 3
-            },
-            shadowRadius: 6,
-            shadowOpacity: 1
-        }
-    }
-})
-
-const MainNavigator = StackNavigator({
-    Home: {
-        screen: Tabs
-    },
-    DeckDetail: {
-        screen: DeckDetail,
-        navigationOptions: {
-            headerTintColor: white,
-            headerStyle: {
-                backgroundColor: purple
-            }
-        }
-    },
-    AddCard: {
-        screen: AddCard,
-        navigationOptions: {
-            headerTintColor: white,
-            headerStyle: {
-                backgroundColor: purple
-            }
-        }
-    },
-    Quiz: {
-        screen: Quiz,
-        navigationOptions: {
-            headerTintColor: white,
-            headerStyle: {
-                backgroundColor: purple
-            }
-        }
-    }
-})
 
 export default class App extends Component {
     render() {

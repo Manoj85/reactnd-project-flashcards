40ada35174c81b82455926271839d1fba0f625ff
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2017-12-26 22:23:54 -0500

    REVIEW: Refactor code

--- components/views/AddDeck.js
+++ components/views/AddDeck.js
@@ -97,10 +97,9 @@ const styles = StyleSheet.create({
     }
 })
 
-function mapStateToProps ({decks}) {
-    return { decks }
-}
+const mapStateToProps = ({ decks }) => ({ decks })
 
 export default connect(mapStateToProps, {
     addDeck
 })(AddDeck)
+
--- components/views/DeckList.js
+++ components/views/DeckList.js
@@ -84,10 +84,6 @@ const styles = StyleSheet.create({
     }
 })
 
-function mapStateToProps ({decks}) {
-    return {
-        decks: decks
-    }
-}
+const mapStateToProps = ({ decks }) => ({ decks })
 
 export default connect(mapStateToProps, {loadDecks})(DeckList)
